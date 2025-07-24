f9bcef225fac8e3b0bdba308ff9153d4198529ac
Eugene Somov <evso7th@gmail.com>
2025-07-24 03:47:12 +0000

    Теперь нормально. Давай немного закруглим края у всех кнопок. совсем нем

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,9 +68,9 @@ export default {
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: `4px`,
+        md: `4px`,
+        sm: `2px`,
       },
       keyframes: {
         'accordion-down': {
