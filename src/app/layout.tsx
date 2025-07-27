1e123db50aaefb725f3882d03a48e90efb295ac6
Eugene Somov <evso7th@gmail.com>
2025-07-27 17:23:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,26 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
+        {/* Yandex.Metrika counter */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103501558', 'ym');
+
+              ym(103501558, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        {/* /Yandex.Metrika counter */}
       </head>
       <body className="font-body antialiased">
+        <noscript><div><img src="https://mc.yandex.ru/watch/103501558" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
         <DisableContextMenu />
         {children}
         <Toaster />
