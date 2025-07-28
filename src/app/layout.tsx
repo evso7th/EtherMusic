59b6b6a801477bebc2c9fba7518a1840444b285e
Eugene Somov <evso7th@gmail.com>
2025-07-28 21:07:15 +0000

    нам надо добавить код счетчика в HTML-код всех страниц сайта. Код нужно

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,20 +25,24 @@ export default function RootLayout({
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(m,e,t,r,i,k,a){
-                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103501558', 'ym');
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-              ym(103501558, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+              ym(XXXXXXXX, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });
             `,
           }}
         />
+        <noscript><div><img src="https://mc.yandex.ru/watch/XXXXXXXX" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
       </head>
       <body className="font-body antialiased">
-        <noscript><div><img src="https://mc.yandex.ru/watch/103501558" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
         <DisableContextMenu />
         {children}
         <Toaster />
