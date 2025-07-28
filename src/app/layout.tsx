ea4832300807dd130534d2836835f6efd2d80806
Eugene Somov <evso7th@gmail.com>
2025-07-28 21:08:44 +0000

    <!-- Yandex.Metrika counter --> <script type="text/javascript">     (fun

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,22 +25,18 @@ export default function RootLayout({
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
 
-              ym(XXXXXXXX, "init", {
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-              });
+              ym(103516734, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
             `,
           }}
         />
-        <noscript><div><img src="https://mc.yandex.ru/watch/XXXXXXXX" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
+        <noscript><div><img src="https://mc.yandex.ru/watch/103516734" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
       </head>
       <body className="font-body antialiased">
         <DisableContextMenu />
