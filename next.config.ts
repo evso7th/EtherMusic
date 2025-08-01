b56bc805f99608ddc7e0ea51a70a2f5d32dd64f9
Eugene Somov <evso7th@gmail.com>
2025-08-01 12:16:31 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -23,19 +23,6 @@ const nextConfig: NextConfig = {
   devIndicators: false,
   // This is needed to allow cross-origin requests in the dev environment.
   allowedDevOrigins: ['**.cloudworkstations.dev'],
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-        config.module.rules.push({
-            test: /autopilot-worker\.ts$/,
-            loader: 'worker-loader',
-            options: {
-                filename: 'static/chunks/[name].[contenthash].js',
-                publicPath: '/_next/',
-            },
-        });
-    }
-    return config;
-  }
 };
 
 export default nextConfig;
