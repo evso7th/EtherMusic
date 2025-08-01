91724db16c31d66948c5c97ba5408a615e8148c7
Eugene Somov <evso7th@gmail.com>
2025-08-01 12:00:58 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,19 @@ const nextConfig: NextConfig = {
   devIndicators: false,
   // This is needed to allow cross-origin requests in the dev environment.
   allowedDevOrigins: ['**.cloudworkstations.dev'],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+        config.module.rules.push({
+            test: /autopilot-worker\.ts$/,
+            loader: 'worker-loader',
+            options: {
+                filename: 'static/chunks/[name].[contenthash].js',
+                publicPath: '/_next/',
+            },
+        });
+    }
+    return config;
+  }
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
