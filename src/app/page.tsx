91da8b760d4752dcbdf492842e6272aec293fa30
Eugene Somov <evso7th@gmail.com>
2025-08-01 18:11:10 +0000

    Надо еще в 2 раза тише..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
     const [isPlaying, setIsPlaying] = useState(false);
     const [isRecording, setIsRecording] = useState(false);
     const [activeTempo, setActiveTempo] = useState<Tempo>(tempos[2]);
-    const [volumes, setVolumes] = useState({ melody: -6, manualBass: -6, latch: -9, drums: -9, autopilot: -12 });
+    const [volumes, setVolumes] = useState({ melody: -6, manualBass: -6, latch: -15, drums: -9, autopilot: -12 });
     const [effects, setEffects] = useState({
         melody: { reverb: -60, delay: -60 },
         manualBass: { reverb: -60, delay: -60 },
