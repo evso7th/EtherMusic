535b88bf49d0cb03733dfeb5110c33cdab8506ca
Eugene Somov <evso7th@gmail.com>
2025-08-04 11:28:46 +0000

    Смотри, это протокол предыдущего успешного деплоя: > nextn@0.1.0 build >

---
+++ src/next.config.ts
@@ -0,0 +1,29 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  devIndicators: false,
+  // This is needed to allow cross-origin requests in the dev environment.
+  allowedDevOrigins: ['**.cloudworkstations.dev'],
+};
+
+export default nextConfig;
