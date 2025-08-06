41ad502a0b36cc524cbfe8531f9d7de0840cc567
Eugene Somov <evso7th@gmail.com>
2025-08-06 06:05:29 +0000

    Уфф. Успокоил :)) как думаешь, какого номера версии достойно сегодняшнее

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -397,7 +397,7 @@ export default function Home() {
                  <footer className="z-10 text-xs text-white/50 pb-4 text-center">
                     <p>Powered by theremin technology</p>
                     <p>&copy; 2025, EVS</p>
-                    <p className="mt-2">v.1.035</p>
+                    <p className="mt-2">v.1.1.0</p>
                 </footer>
                 <CookieConsent />
             </div>
