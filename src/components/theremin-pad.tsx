3d7942c6c98755f3251194a5e3ebac23567d0924
Eugene Somov <evso7th@gmail.com>
2025-09-09 18:21:49 +0000

    проверь структуру файлов проекта еще раз

--- src/components/theremin-pad.tsx
+++ src/components/theremin-pad.tsx
@@ -349,9 +349,11 @@ export function ThereminPad({
                     }}
                 >
                     <div className="absolute inset-0 flex items-center justify-center text-5xl md:text-7xl font-bold text-foreground/10 pointer-events-none uppercase tracking-widest text-center leading-tight">
-                        <div className="md:hidden" dangerouslySetInnerHTML={{ __html: padTitle.replace(' ', '<br/>') }} />
+                        <div className="md:hidden">
+                            {title}<br />{subtitle}
+                        </div>
                         <div className="hidden md:block">
-                           <span>{padTitle}</span>
+                           <span>{title} {subtitle}</span>
                         </div>
                     </div>
                 </div>
