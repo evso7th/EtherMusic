1f82974edf14dcb17aaca003a52e7efd091f9e20
Eugene Somov <evso7th@gmail.com>
2025-09-13 06:41:26 +0000

    никаких логов не появилось. я предлагаю следующее - перенести весь код л

--- src/lib/latch-processor.js
+++ src/lib/latch-processor.js
@@ -1,3 +1,18 @@
 // This AudioWorkletProcessor is DEPRECATED and no longer used.
 // The 'latch' synth channel now uses the main 'synth-processor.js'.
 // This file is kept for historical reference and can be safely removed.
+class DeprecatedLatchProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.port.onmessage = () => {
+      // No-op
+    };
+  }
+
+  process(inputs, outputs, parameters) {
+    // Return false to let the processor be garbage-collected.
+    return false;
+  }
+}
+
+registerProcessor('latch-processor', DeprecatedLatchProcessor);
